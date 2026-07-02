18ad526e6a7c15be82718eb4cef43b2cd3437c31
root <root@Rocket.localdomain>
2026-07-02 01:59:20 +0000

    added conflict change for go

--- dummy.go
+++ dummy.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World! Your Go application is running successfully.")
+	fmt.Println("Hello, World! Your Go application is running successfully. I want to test dummy")
 }
